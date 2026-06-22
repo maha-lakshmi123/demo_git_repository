c67bdf6a9ec3d1d0dcb2e8f2c3244a985add11b3
Maha <mahalakshmi25032005@gmail.com>
2026-06-22 08:17:28 +0530

    Change in uit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -47,14 +47,6 @@ public class Main{
                 .forEach(System.out::println);
 
 
-        System.out.printf("After change");
-
-        System.out.printf("After change");
-
-
-        System.out.println("Maya");
-
-        System.out.println("Lakshmi");
 
 
 
